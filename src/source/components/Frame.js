c2e45fe5eca3a196eafceb1c66e7fb753a5485d9
afrontend <autoscripts@gmail.com>
2021-04-07 21:45:37 +0900

    Use toJSONString instead of JSON.stringify

--- src/source/components/Frame.js
+++ src/source/components/Frame.js
@@ -2,19 +2,19 @@ import React, { useState } from 'react';
 import Logo from './Logo'
 import TextPanel from './TextPanel'
 import Button from './Button'
-import { textPathsAryToObj } from 'ary2json'
+import { textPathsAryToObj, toJSONString} from 'ary2json'
 
 function Frame () {
   const [csvText, setCSVText] = useState('Wrong password, 잘못된 패스워드, errorMsg, invalidPassword');
   const [jsonString, setJsonString] = useState('');
 
   function convert() {
-    setJsonString(JSON.stringify(textPathsAryToObj([csvText.split(',')], {numberOfLanguageColumn: 2}), null, 2))
+    setJsonString(toJSONString(textPathsAryToObj([csvText.split(',')], {numberOfLanguageColumn: 2})))
   }
 
   return <div className="Frame">
     <Logo />
-    <TextPanel value={csvText} onChange={(e) => setCSVText(e.target.value)} laceholder="CSV Text" />
+    <TextPanel value={csvText} onChange={(e) => setCSVText(e.target.value)} placeholder="CSV Text" />
     <Button onClick={convert} />
     <TextPanel value={jsonString} placeholder="JSON Object" />
   </div>
