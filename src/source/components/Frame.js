42811e5d10209bfed0e8e7f69f3c2bb6ff3206b5
afrontend <autoscripts@gmail.com>
2021-04-08 22:42:48 +0900

    Add <select> for choice a number of language column

--- src/source/components/Frame.js
+++ src/source/components/Frame.js
@@ -4,19 +4,35 @@ import TextPanel from './TextPanel'
 import Button from './Button'
 import { textPathsAryToObj, toJSONString} from 'ary2json'
 
+const compose = (...fns) =>
+  (arg) => (fns.reduce((composed, f) => f(composed) ,arg))
+
 function Frame () {
   const [csvText, setCSVText] = useState('Wrong password, 잘못된 패스워드, errorMsg, invalidPassword');
   const [jsonString, setJsonString] = useState('');
+  const [numberOfLanguageColumn, setNumberOfLanguageColumn] = useState(2);
 
-  function convert() {
-    setJsonString(toJSONString(textPathsAryToObj([csvText.split(',')], {numberOfLanguageColumn: 2})))
-  }
+  const convert = compose(
+    (csv) => textPathsAryToObj([csv.split(',')], {numberOfLanguageColumn}),
+    toJSONString,
+    setJsonString
+  )
 
   return <div className="Frame">
     <Logo />
     <TextPanel value={csvText} onChange={(e) => setCSVText(e.target.value)} placeholder="CSV Text" />
-    <Button onClick={convert} />
-    <TextPanel value={jsonString} placeholder="JSON Object" />
+    <span>
+      a number of language column:
+    </span>
+    <select name="a number of language column" value={numberOfLanguageColumn} onChange={(e) => setNumberOfLanguageColumn(+e.target.value)}>
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+    </select>
+    <Button onClick={() => convert(csvText)} />
+    <TextPanel value={jsonString} onChange={e => e} placeholder="JSON Object" />
   </div>
 }
 
